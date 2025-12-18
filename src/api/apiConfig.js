a159e16bbeff100162a7c909deb0269c1362e852
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-12-18 10:04:39 +0530

    changes added

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api=axios.create({
-    baseURL:"https://backend-school-3xgd.onrender.com/api" //backend url
+     baseURL:"https://backend-school-3xgd.onrender.com/api"  
+    /* baseURL:" http://localhost:5000/api" */  //backend url
 })
 
 //add a token to the request headers
